b20c470166c95ec84f93ba94258140f040f93545
Lee Warnock <clwarnock@gmail.com>
2018-01-07 17:04:06 -0600

    add lesson 3 hello.py

---
+++ Lesson3/hello.py.py
@@ -0,0 +1,11 @@
+# This program says hello and asks for my name
+
+print('Hello world!')
+print ('What is your name?') #Ask for thier name
+myName = input()
+print ('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(MyName))
+print('What is your age?') # ask for thier age
+my Age = input()
+print('You will be ' + str(int(myAge) = 1) + ' "in a year.')
