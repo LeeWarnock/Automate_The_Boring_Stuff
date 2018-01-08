ebb5b3838ff359cc974a31fdfcadc7f50c66f2f7
Lee Warnock <clwarnock@gmail.com>
2018-01-07 19:37:25 -0600

    linted

--- Lesson3/hello.py.py
+++ Lesson3/hello.py.py
@@ -1,11 +1,11 @@
 # This program says hello and asks for my name
 
 print('Hello world!')
-print ('What is your name?') #Ask for thier name
+print('What is your name?') #Ask for their name
 myName = input()
-print ('It is good to meet you, ' + myName)
+print('It is good to meet you, ' + myName)
 print('The length of your name is:')
-print(len(MyName))
-print('What is your age?') # ask for thier age
-my Age = input()
-print('You will be ' + str(int(myAge) = 1) + ' in a year.')
+print(len(myName))
+print('What is your age?') # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
